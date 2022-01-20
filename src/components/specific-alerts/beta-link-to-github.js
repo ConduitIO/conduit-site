d3dfcd51bb1d75a1b568f3b30a3af7f2dc1bec96
Taron Foxworth <taronfoxworth@gmail.com>
2022-01-20 11:24:26 -0500

    update: launch feedback part 4

---
+++ src/components/specific-alerts/beta-link-to-github.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Alert from '../Alert';
+
+export default function BetaAlert({ title, list }) {
+    
+    return (
+        <Alert title={"Hey, this thing is in beta [will fix messaging here]"} />
+    )
+}
