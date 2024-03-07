05bafc672a2241bfb90b60cff854f931cd3309b2
Haris Osmanagic <haris@meroxa.io>
2024-03-07 12:31:59 +0100

    Banner for 0.9

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -127,7 +127,7 @@ const config: Config = {
     },
     announcementBar: {
       id: 'announcement-bar-1', // increment on change
-      content: `Conduit 0.8 is here! <a class='cta' href='https://github.com/ConduitIO/conduit/releases/latest' target='_blank' rel='noreferrer noopener'>See what's new</a>.`,
+      content: `Conduit 0.9 is here! <a class='cta' href='https://github.com/ConduitIO/conduit/releases/latest' target='_blank' rel='noreferrer noopener'>See what's new</a>.`,
       isCloseable: true,
     },
     colorMode: {
