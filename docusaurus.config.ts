01c1724f4f7c5396883c73fc1771ef8062586c4f
Haris Osmanagić <haris@meroxa.io>
2024-03-07 15:30:28 +0100

    Update docusaurus.config.ts
    Co-authored-by: Lovro Mažgon <lovro.mazgon@gmail.com>

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -126,7 +126,7 @@ const config: Config = {
       copyright: `Copyright © ${new Date().getFullYear()} Meroxa, Inc.`,
     },
     announcementBar: {
-      id: 'announcement-bar-1', // increment on change
+      id: 'announcement-bar-2', // increment on change
       content: `Conduit 0.9 is here! <a class='cta' href='https://github.com/ConduitIO/conduit/releases/latest' target='_blank' rel='noreferrer noopener'>See what's new</a>.`,
       isCloseable: true,
     },
