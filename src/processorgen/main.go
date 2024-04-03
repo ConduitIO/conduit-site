991ded8f24011331a0cd56577d7b545b7160a529
Lovro Mažgon <lovro.mazgon@gmail.com>
2024-04-03 16:50:26 +0200

    display processor configuration as yaml

--- src/processorgen/main.go
+++ src/processorgen/main.go
@@ -106,12 +106,14 @@ func exportProcessorDoc(index int, inputPath, outputPath string, t *template.Tem
 }
 
 var funcMap = template.FuncMap{
-	"formatParameterValue": formatParameterValue,
-	"formatRecord":         formatRecord,
+	"formatParameterValueTable":      formatParameterValueTable,
+	"formatParameterValueYAML":       formatParameterValueYAML,
+	"formatParameterDescriptionYAML": formatParameterDescriptionYAML,
+	"formatRecord":                   formatRecord,
 }
 
 // formatParameterValue formats the value of a configuration parameter.
-func formatParameterValue(value string) string {
+func formatParameterValueTable(value string) string {
 	switch {
 	case value == "":
 		return `<Chip label="null" />`
@@ -120,6 +122,78 @@ func formatParameterValue(value string) string {
 		return fmt.Sprintf("\n```js\n%s\n```\n", value)
 	default:
 		return fmt.Sprintf("`%s`", value)
+	}
+}
+
+func formatParameterDescriptionYAML(description string) string {
+	const (
+		indentLen  = 10
+		prefix     = "# "
+		lineLen    = 80
+		tmpNewLine = "〠"
+	)
+
+	// remove markdown new lines
+	description = strings.ReplaceAll(description, "\n\n", tmpNewLine)
+	description = strings.ReplaceAll(description, "\n", " ")
+	description = strings.ReplaceAll(description, tmpNewLine, "\n")
+
+	formattedDescription := formatMultiline(description, strings.Repeat(" ", indentLen)+prefix, lineLen)
+	// remove first indent and last new line
+	formattedDescription = formattedDescription[indentLen : len(formattedDescription)-1]
+	return formattedDescription
+}
+
+func formatMultiline(
+	input string,
+	prefix string,
+	maxLineLen int,
+) string {
+	textLen := maxLineLen - len(prefix)
+
+	// split the input into lines of length textLen
+	lines := strings.Split(input, "\n")
+	var formattedLines []string
+	for _, line := range lines {
+		if len(line) <= textLen {
+			formattedLines = append(formattedLines, line)
+			continue
+		}
+
+		// split the line into multiple lines, don't break words
+		words := strings.Fields(line)
+		var formattedLine string
+		for _, word := range words {
+			if len(formattedLine)+len(word) > textLen {
+				formattedLines = append(formattedLines, formattedLine[1:])
+				formattedLine = ""
+			}
+			formattedLine += " " + word
+		}
+		if formattedLine != "" {
+			formattedLines = append(formattedLines, formattedLine[1:])
+		}
+	}
+
+	// combine lines including indent and prefix
+	var formatted string
+	for _, line := range formattedLines {
+		formatted += prefix + line + "\n"
+	}
+
+	return formatted
+}
+
+func formatParameterValueYAML(value string) string {
+	switch {
+	case value == "":
+		return `""`
+	case strings.Contains(value, "\n"):
+		// specifically used in the javascript processor
+		formattedValue := formatMultiline(value, "            ", 10000)
+		return fmt.Sprintf("|\n%s", formattedValue)
+	default:
+		return fmt.Sprintf(`"%s"`, value)
 	}
 }
 
