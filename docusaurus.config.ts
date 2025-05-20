4767f84b9043e7feeddf814671e44899244f64e9
Raúl Barroso <code@raulb.dev>
2025-05-20 14:08:42 +0200

    fix link

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -163,7 +163,7 @@ const config: Config = {
     },
     announcementBar: {
       id: 'announcement-bar-12', // increment on change
-      content: `Conduit v0.13.5 is here! <a class='cta' href='/changelog/2025-05-19-conduit-0-13-5-release' target='_blank' rel='noreferrer noopener'>See what's new</a>.`,
+      content: `Conduit v0.13.5 is here! <a class='cta' href='/changelog/2025-05-20-conduit-0-13-5-release' target='_blank' rel='noreferrer noopener'>See what's new</a>.`,
       isCloseable: true,
     },
     colorMode: {
